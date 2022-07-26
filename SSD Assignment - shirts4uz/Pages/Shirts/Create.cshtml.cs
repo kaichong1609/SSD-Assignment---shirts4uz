09e4efa7b1f82968981ebfd994ffd5a21619b38d
Kai Chong Lim <s10226797@connect.np.edu.sg>
2022-07-26 17:03:44 +0800

    Preventing overposting by adding the TryUpdateModelAsync method

--- SSD Assignment - shirts4uz/Pages/Shirts/Create.cshtml.cs
+++ SSD Assignment - shirts4uz/Pages/Shirts/Create.cshtml.cs
@@ -40,32 +40,36 @@ namespace SSD_Assignment___shirts4uz.Pages.Shirts
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var emptyShirt = new Shirt();
+            if (await TryUpdateModelAsync<Shirt>(emptyShirt, "shirt", s => s.Name, s => s.Color, s => s.Size, s => s.Description, s => s.Price
+                , s => s.ListDate, s => s.Category, s => s.PhotoPath))
             {
-                return Page();
+                if (!ModelState.IsValid)
+                {
+                    return Page();
+                }
+                _context.Shirt.Add(Shirt);
+                //await _context.SaveChangesAsync();
+                // Once a record is added, create an audit record
+                if (await _context.SaveChangesAsync() > 0)
+                {
+                    // Create an auditrecord object
+                    var auditrecord = new AuditRecord();
+                    auditrecord.AuditActionType = "Add Shirt Record";
+                    auditrecord.DateTimeStamp = DateTime.Now;
+                    auditrecord.KeyShirtFieldID = Shirt.ID.ToString();
+                    // Get current logged-in user
+                    var userID = User.Identity.Name.ToString();
+                    auditrecord.Username = userID;
+                    _context.AuditRecords.Add(auditrecord);
+                    await _context.SaveChangesAsync();
+                }
+
+                _context.Shirt.Add(Shirt);
+                return RedirectToPage("./Index");
             }
+            return Page();
 
-            _context.Shirt.Add(Shirt);
-            //await _context.SaveChangesAsync();
-            // Once a record is added, create an audit record
-            if (await _context.SaveChangesAsync() > 0)
-            {
-                // Create an auditrecord object
-                var auditrecord = new AuditRecord();
-                auditrecord.AuditActionType = "Add Shirt Record";
-                auditrecord.DateTimeStamp = DateTime.Now;
-                auditrecord.KeyShirtFieldID = Shirt.ID.ToString();
-                // Get current logged-in user
-                var userID = User.Identity.Name.ToString();
-                auditrecord.Username = userID;
-                _context.AuditRecords.Add(auditrecord);
-                await _context.SaveChangesAsync();
-            }
-
-
-            _context.Shirt.Add(Shirt);
-
-            return RedirectToPage("./Index");
         }
     }
 }
