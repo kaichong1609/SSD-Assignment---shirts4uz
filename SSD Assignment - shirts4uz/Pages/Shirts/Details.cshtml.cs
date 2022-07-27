b8a87c8d21cfcea72fa6b82b664bca99eea3fa0a
Kai Chong Lim <s10226797@connect.np.edu.sg>
2022-07-27 12:06:31 +0800

    Fixed dropdown list to properly direct users to specified shirt category

--- SSD Assignment - shirts4uz/Pages/Shirts/Details.cshtml.cs
+++ SSD Assignment - shirts4uz/Pages/Shirts/Details.cshtml.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
 using SSD_Assignment___shirts4uz.Data;
 using SSD_Assignment___shirts4uz.Models;
 
 namespace SSD_Assignment___shirts4uz.Pages.Shirts
 {
-    [Authorize]
     public class DetailsModel : PageModel
     {
         private readonly SSD_Assignment___shirts4uz.Data.SSD_Assignment___shirts4uzContext _context;
